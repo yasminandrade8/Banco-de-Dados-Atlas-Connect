21a241c7a166d030a27a3303bb4397a2b031ddd1
Yasmin Rodrigues de Andrade <yasminrandrade014@gmail.com>
2026-04-23 19:38:23 -0300

    Update consultas.sql

--- Database/consultas.sql
+++ Database/consultas.sql
@@ -1,6 +1,26 @@
--- Tabelas --
+-- Consultas básicas --
 SELECT * FROM Usuario;
 SELECT * FROM Consultor;
+SELECT * FROM Mentoria;
+
+SELECT * FROM Consultor
+WHERE status = 'Aprovado';
+
+SELECT COUNT(*) AS TotalCandidatos FROM Usuario;
+
+SELECT status, COUNT(*) AS 'Total por status'
+FROM Mentoria
+GROUP BY status;
+
+SELECT 
+    m.idMentoria,
+    u.nomeUsuario AS Candidato,
+    c.nomeConsultor AS Consultor,
+    m.dataAgendamento
+FROM Mentoria m
+INNER JOIN Usuario u ON m.idUsuario = u.idUsuario
+INNER JOIN Consultor c ON m.idConsultor = c.idConsultor
+ORDER BY m.idMentoria DESC;
 
 -- Tabelas de Log --
 SELECT * FROM dateLogUser;
